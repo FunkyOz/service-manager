b11af5fbe5a6aeeb2d5407f3fceb21d06f43af47
Lorenzo Dessimoni <lorenzo.dessimoni@corley.it>
2023-06-15 15:44:43 +0200

    fix problem with migration and seeders

--- bin/service-manager.js
+++ bin/service-manager.js
@@ -14,6 +14,15 @@ function parseServices(options) {
   return services
 }
 
+function parseMigrateAndSeedOptions(options) {
+  const { migrate = false, seed = false } = options ?? {}
+
+  return {
+    withMigration: migrate,
+    withSeed: seed
+  }
+}
+
 (async function() {
   program.version('1.0.0')
 
@@ -21,20 +30,22 @@ function parseServices(options) {
     .command('start')
     .description('start a new docker service using docker-compose')
     .argument('<service>', 'service name')
-    .option('--migration', 'should run migration', false)
+    .option('--migrate', 'should run migration', false)
     .option('--seed', 'should run seed', false)
     .action(async(service, options) => {
-      await require('../.').start(service, options)
+      await require('../.').start(service, parseMigrateAndSeedOptions(options))
     })
 
   program.command('start:all')
     .description('Start all services inside project direcotory')
     .option('--services <services...>', 'comma separated services to start', [])
     .option('--rollback-on-error', 'rollback services when an error occurred', false)
-    .option('--migration', 'should run migration', false)
+    .option('--migrate', 'should run migration', false)
     .option('--seed', 'should run seed', false)
     .action(async(options) => {
       const services = parseServices(options)
+
+      options = parseMigrateAndSeedOptions(options)
 
       await require('..').startAll(services, options)
     })
